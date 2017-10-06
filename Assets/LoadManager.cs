1426b9744273378da30f31b9016006b252f909e2
hori <hori@hori-no-Mac-mini.local>
2017-10-06 16:11:58 +0900

    scene load

--- Assets/LoadManager.cs
+++ Assets/LoadManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UniRx;
 using UniRx.Triggers;
 using System;
+using UnityEngine.SceneManagement;
 
 public class LoadManager : MonoBehaviour {
 
@@ -20,6 +21,11 @@ public class LoadManager : MonoBehaviour {
 			Debug.Log("change progress");
 			progressBar.fillAmount = progress.Value;
 		});
+		progress
+			.Where(x => progress.Value >= 1.0f)
+			.Subscribe(x => {
+				showMainScene();
+			});
 
 		this.ObserveEveryValueChanged (x => uiController.loadStart)
 			.Where(x => x)
@@ -37,20 +43,31 @@ public class LoadManager : MonoBehaviour {
 			.Delay(TimeSpan.FromMilliseconds(2000))
 			.Subscribe(_ => {
 				Debug.Log("change progress2");
-				progress.Value = 0.2f;
+				progress.Value = 0.4f;
 			});
 		Observable.Return(Unit.Default)
 			.Delay(TimeSpan.FromMilliseconds(3000))
 			.Subscribe(_ => {
 				Debug.Log("change progress3");
-				progress.Value = 0.3f;
+				progress.Value = 0.6f;
 			});
 		Observable.Return(Unit.Default)
 			.Delay(TimeSpan.FromMilliseconds(4000))
 			.Subscribe(_ => {
 				Debug.Log("change progress4");
-				progress.Value = 0.4f;
+				progress.Value = 0.8f;
 			});
+		Observable.Return(Unit.Default)
+			.Delay(TimeSpan.FromMilliseconds(4000))
+			.Subscribe(_ => {
+				Debug.Log("change progress5");
+				progress.Value = 1.0f;
+			});
+	}
+
+	void showMainScene(){
+		Debug.Log("LoadScene");
+		SceneManager.LoadScene("MapScene");
 	}
 
 	// Update is called once per frame
